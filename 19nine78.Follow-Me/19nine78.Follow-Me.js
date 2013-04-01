c43d353154271a9013f4ac34293b9a25a84f29aa
19nine78 <19nine78@gmail.com>
2013-04-01 20:52:55 +0100

    Really simple jQuery plugin

---
+++ 19nine78.Follow-Me/19nine78.Follow-Me.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+    $.fn.followMe = function (options) {
+
+        var settings = $.extend({ 'follower': $('.subContainer') }, options);
+
+        this.each(function () {
+
+            $(this).on('mouseover', function () {
+                var element = settings.follower.detach();
+                var target = $(this);
+                target.append(element);
+            });
+
+        });
+
+    };
+})(jQuery);
